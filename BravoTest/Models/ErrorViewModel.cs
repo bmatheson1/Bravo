2ace341b0214f66ec26729cecc1fd4a4161047a8
Brodie Matheson <bmatheson@jhg.com.au>
2020-06-12 16:45:10 +1000

    .NET Core MVC web app

---
+++ BravoTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BravoTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
