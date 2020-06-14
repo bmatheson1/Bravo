1ffe277d7f4891c588ce818a39c26c1b0cd22ac0
Brodie Matheson <bmatheson@jhg.com.au>
2020-06-14 19:33:10 +1000

    Added very basic test

---
+++ test/Tests/Test.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Project.Models;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        //
+        // todo: A lot more tests...
+        //
+
+        private readonly ICorporateJargonGenerator _corporateJargonGenerator;
+
+        public Test()
+        {
+            _corporateJargonGenerator = new CorporateJargonGenerator();
+        }
+
+        [Test]
+        public void GetFluff_ShouldGenerateJargon()
+        {
+            string result = _corporateJargonGenerator.GetFluff();
+
+            Assert.IsNotEmpty(result);
+        }
+
+
+    }
+}
